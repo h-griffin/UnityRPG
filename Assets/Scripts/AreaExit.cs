2813bb27093289603531f18351136e38c79098b7
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-12-01 20:33:07 -0800

    first commit

---
+++ Assets/Scripts/AreaExit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;   // <<<<<<<
+
+public class AreaExit : MonoBehaviour
+{
+
+    public string areaToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void onTriggerEnter2D(Collider2D other)
+    {
+        //if (other.gameObject.tag == "Player")
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(areaToLoad);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // MOVEMENT
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+
+    // ANIMATION
+    public Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+        myAnim.SetFloat("moveX", theRB.velocity.x);
+        myAnim.SetFloat("moveY", theRB.velocity.y);
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1 )
+        {
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+
+    }
+}

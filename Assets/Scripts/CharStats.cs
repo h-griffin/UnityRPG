9c223b1d08537f0efc801e6bfe313e52933e7556
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-12-03 23:45:15 -0800

    level up

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+
+    public string charName;
+    public int playerLevel = 1;
+    public int currentEXP;
+
+    public int[] expToNextLevel;
+    public int maxLevel = 100;
+    public int baseEXP = 1000;
+
+    public int currentHP;
+    public int maxHP = 100;
+    public int currentMP;
+    public int maxMP = 30;
+    public int strength;
+    public int defense;
+    public int wpnPwr;
+    public int armrPwr;
+    public string equippedWpn;
+    public string equippedArmr;
+    public Sprite charImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        expToNextLevel = new int[maxLevel];
+        expToNextLevel[1] = baseEXP;
+
+        for (int i = 2; i < expToNextLevel.Length; i++)
+        {
+            expToNextLevel[i] = Mathf.FloorToInt(expToNextLevel[i - 1] * 1.05f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            AddExp(500);
+        }
+    }
+
+    public void AddExp(int expToAdd)
+    {
+        currentEXP += expToAdd;
+
+        if (currentEXP > expToNextLevel[playerLevel])
+        {
+            currentEXP -= expToNextLevel[playerLevel];
+
+            playerLevel++;
+        }
+    }
+}

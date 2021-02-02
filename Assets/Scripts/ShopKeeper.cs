f6faf0fcabe63a9746a659291dc8dfea388d3dd5
Griffin <60614863+h-griffin@users.noreply.github.com>
2021-02-01 18:07:36 -0800

    shop items

---
+++ Assets/Scripts/ShopKeeper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopKeeper : MonoBehaviour
+{
+
+    private bool canOpen;
+    public string[] itemsForSale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // player at table and not in other menus
+        if(canOpen && Input.GetButtonDown("Fire1") && PlayerController.instance.canMove && !Shop.instance.shopMenu.activeInHierarchy)
+        {
+            Shop.instance.itemsForSale = itemsForSale;
+
+            Shop.instance.OpenShop();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            canOpen = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canOpen = false;
+        }
+    }
+}

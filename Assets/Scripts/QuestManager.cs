6c7a6c91cbe1db30162b81e946ef6f1576972980
Griffin <60614863+h-griffin@users.noreply.github.com>
2021-02-02 17:57:13 -0800

    quest objcets

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -13,11 +13,62 @@ public class QuestManager : MonoBehaviour
     void Start()
     {
         instance = this;
+
+        questMarkersComplete = new bool[questMarkerNames.Length];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log(CheckIfComplete("quest test"));
+        }
+    }
+
+    public int GetQuestNumber(string questToFind)
+    {
+        for(int i = 0; i < questMarkerNames.Length; i++)
+        {
+            if(questMarkerNames[i] == questToFind)
+            {
+                return i;
+            }
+        }
+        Debug.LogError("Quest < " + questToFind + " > does not exist");
+        return 0;
+    }
+
+    public bool CheckIfComplete(string questToCheck)
+    {
+        if(GetQuestNumber(questToCheck) != 0)
+        {
+            return questMarkersComplete[GetQuestNumber(questToCheck)];
+        }
+        return false;
+    }
+
+    public void MarkQuestComplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = true;
+        UpdateLocalQuestObjects();
+    }
+
+    public void MarkQuestIncomplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = false;
+        UpdateLocalQuestObjects();
+    }
+
+    public void UpdateLocalQuestObjects()
+    {
+        QuestObjectActivator[] questObjects = FindObjectsOfType<QuestObjectActivator>();
+        if(questObjects.Length > 0)
+        {
+            for(int i = 0; i < questObjects.Length; i++)
+            {
+                questObjects[i].CheckCompletion();
+            }
+        }
     }
 }
